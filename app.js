4e7531c314e4419a94b9d7fd3b5b83798cadb00b
Tiya Lee <Bigtiyalee@gmail.com>
2021-08-12 12:59:52 -0400

    Schemas

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const morgan = require("morgan");
+const layout = require('./views/layout');
+const {db} = require('./models');
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: false }));
 
+
+// db.authenticate()
+//   .then(() => {
+//     console.log('connected to the database');
+//   })
+
 app.get("/", (req, res) => {
-  res.send('Hello World');
+  res.send(layout(''));
 })
 
-const PORT = 1337;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log('WORKING');
